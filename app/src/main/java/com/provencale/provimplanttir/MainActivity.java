9256d608fe2d24841cb5d9498130868547829703
Jean-Baptiste Laffitte <jb.laffitte@provencale.com>
2023-09-11 12:37:12 +0200

    minor: correct a bug saving twice the latitude instead of latitude/longitude

--- app/src/main/java/com/provencale/provimplanttir/MainActivity.java
+++ app/src/main/java/com/provencale/provimplanttir/MainActivity.java
@@ -573,7 +573,7 @@ public class MainActivity extends AppCompatActivity {
                 int numeroTrou = mNumeroTrouDansRangeeNumberPicker.getValue();
                 Log.v("registerLastPositionAsTrou", "Avant : " + volees.toString());
                 volees.addtrou(nomVolee, numeroRangee, numeroTrou,
-                        mLastLocation.getLatitude(), mLastLocation.getLatitude(), mLastLocation.getAltitude(),
+                        mLastLocation.getLatitude(), mLastLocation.getLongitude(), mLastLocation.getAltitude(),
                         mLastLocation.getTime());
                 Log.v("registerLastPositionAsTrou", "Apres : " + volees.toString());
                 volees.write(getApplicationContext());
